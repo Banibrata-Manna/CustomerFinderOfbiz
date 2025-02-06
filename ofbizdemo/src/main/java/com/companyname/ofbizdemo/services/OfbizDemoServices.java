a1e2324bda9cbc64796210eaaa853d3f47de0904
Banibrata-Manna <mbanibrata668@gmail.com>
2025-02-06 10:39:33 +0530

    Initial Commit

---
+++ ofbizdemo/src/main/java/com/companyname/ofbizdemo/services/OfbizDemoServices.java
@@ -0,0 +1,33 @@
+package com.companyname.ofbizdemo.services;
+import java.util.Map;
+
+import org.apache.ofbiz.base.util.Debug;
+import org.apache.ofbiz.entity.Delegator;
+import org.apache.ofbiz.entity.GenericEntityException;
+import org.apache.ofbiz.entity.GenericValue;
+import org.apache.ofbiz.service.DispatchContext;
+import org.apache.ofbiz.service.ServiceUtil;
+
+public class OfbizDemoServices {
+
+    public static final String MODULE = OfbizDemoServices.class.getName();
+
+    public static Map<String, Object> createOfbizDemo(DispatchContext dispatchContext, Map<String, ? extends Object> context){
+        Map<String, Object> result = ServiceUtil.returnSuccess();
+        Delegator delegator = dispatchContext.getDelegator();
+        try {
+            GenericValue ofbizDemo = delegator.makeValue("OfbizDemo");
+            ofbizDemo.setNextSeqId();
+            ofbizDemo.setNonPKFields(context);
+            ofbizDemo = delegator.create(ofbizDemo);
+            result.put("ofbizDemoId", ofbizDemo.getString("ofbizDemoId"));
+            Debug.log("/////////////////////////////////////////////////////////Record "
+                    + ofbizDemo.getString("ofbizDemoId") + " Created");
+
+        } catch (GenericEntityException e) {
+            Debug.logError(e, MODULE);
+            return ServiceUtil.returnError("Failed to create record" + MODULE);
+        }
+        return result;
+    }
+}
