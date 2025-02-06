d5beea9d00511db1bf2c0a91cfd73ccb59a85e05
Banibrata-Manna <mbanibrata668@gmail.com>
2025-02-06 12:13:52 +0530

    Groovy Script Tut

---
+++ ofbizdemo/groovyScripts/OfbizDemoServices.groovy
@@ -0,0 +1,23 @@
+import java.util.Map;
+
+import org.apache.ofbiz.base.util.Debug;
+import org.apache.ofbiz.entity.Delegator;
+import org.apache.ofbiz.entity.GenericEntityException;
+import org.apache.ofbiz.entity.GenericValue;
+import org.apache.ofbiz.service.DispatchContext;
+import org.apache.ofbiz.service.ServiceUtil;
+
+def createOfbizDemoServiceByGroovy() {
+  result = [:];
+  try{
+    ofbizDemo = delegator.makeValue("OfbizDemo");
+    ofbizDemo.setNextSeqId();
+    ofbizDemo.setNonPKFields(context);
+    ofbizDemo = delegator.create(ofbizDemo);
+    result.put("ofbizDemoId", ofbizDemo.ofbizDemoId);
+    logInfo("////////////////////////////Created " + ofbizDemo.ofbizDemoId + " ////////////////////////////////////")
+  } catch (GenericEntityException e) {
+    logError(e.getMessage());
+  }
+  return result;
+}
--- ofbizdemo/src/main/java/com/companyname/ofbizdemo/services/OfbizDemoServices.java
+++ ofbizdemo/src/main/java/com/companyname/ofbizdemo/services/OfbizDemoServices.java
@@ -1,4 +1,5 @@
 package com.companyname.ofbizdemo.services;
+
 import java.util.Map;
 
 import org.apache.ofbiz.base.util.Debug;
@@ -10,24 +11,24 @@ import org.apache.ofbiz.service.ServiceUtil;
 
 public class OfbizDemoServices {
 
-    public static final String MODULE = OfbizDemoServices.class.getName();
+  public static final String MODULE = OfbizDemoServices.class.getName();
 
-    public static Map<String, Object> createOfbizDemo(DispatchContext dispatchContext, Map<String, ? extends Object> context){
-        Map<String, Object> result = ServiceUtil.returnSuccess();
-        Delegator delegator = dispatchContext.getDelegator();
-        try {
-            GenericValue ofbizDemo = delegator.makeValue("OfbizDemo");
-            ofbizDemo.setNextSeqId();
-            ofbizDemo.setNonPKFields(context);
-            ofbizDemo = delegator.create(ofbizDemo);
-            result.put("ofbizDemoId", ofbizDemo.getString("ofbizDemoId"));
-            Debug.log("/////////////////////////////////////////////////////////Record "
-                    + ofbizDemo.getString("ofbizDemoId") + " Created");
+  public static Map<String, Object> createOfbizDemo(DispatchContext dispatchContext, Map<String, ? extends Object> context) {
+    Map<String, Object> result = ServiceUtil.returnSuccess();
+    Delegator delegator = dispatchContext.getDelegator();
+    try {
+      GenericValue ofbizDemo = delegator.makeValue("OfbizDemo");
+      ofbizDemo.setNextSeqId();
+      ofbizDemo.setNonPKFields(context);
+      ofbizDemo = delegator.create(ofbizDemo);
+      result.put("ofbizDemoId", ofbizDemo.getString("ofbizDemoId"));
+      Debug.log("/////////////////////////////////////////////////////////Record "
+          + ofbizDemo.getString("ofbizDemoId") + " Created");
 
-        } catch (GenericEntityException e) {
-            Debug.logError(e, MODULE);
-            return ServiceUtil.returnError("Failed to create record" + MODULE);
-        }
-        return result;
+    } catch (GenericEntityException e) {
+      Debug.logError(e, MODULE);
+      return ServiceUtil.returnError("Failed to create record" + MODULE);
     }
+    return result;
+  }
 }
